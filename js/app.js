740bb8cccb6ee3d679c9a2628765775770a4e2a5
isnamdnazim <isnamdnazim@gmail.com>
2021-09-03 01:26:04 +0600

    still working

--- js/app.js
+++ js/app.js
@@ -2,17 +2,19 @@ const API_KEY = `3077b8530089d4e162d071e6e83b154f`;
 
 const temperatureSearch = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => displayTemperature(data))
 }
 
-const setHtmlInnerText = text => {
-
+const setHtmlInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
 }
 
 const displayTemperature = weather => {
     console.log(weather);
+    setHtmlInnerText('show-city-name', weather.name);
+    setHtmlInnerText('temperature', weather.main.temp);
 }
